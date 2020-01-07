19dd7baf937ffae9c4a346baa9fcbd2378dd7def
qsaleh <qmsaleh@gmail.com>
2020-01-07 00:38:04 +0000

    function that asserts if two arguments are equal

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("☠️ ☠️ ☠️ Assertion Failed: \"" + actual + "\" !== \"" + expected + "\"");
+  } else
+    console.log("😀 😀 😀 Assertion Passed: " + actual + " === " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
