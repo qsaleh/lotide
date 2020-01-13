33945bee9551a34cca4302e77201c74cf19e496c
qsaleh <qmsaleh@gmail.com>
2020-01-13 19:49:43 +0000

    created test file and modified assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,5 @@
-const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) return false;
-  for (let i = 0; i <= firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) return false;
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   if (!eqArrays(actual, expected)) {
     console.log(`☠️ ☠️ ☠️ Assertion Failed: "${actual}" !== "${expected}"`);
@@ -12,8 +7,4 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`😀 😀 😀 Assertion Passed: "${actual}" === "${expected}"`);
 };
 
-assertArraysEqual([1,2,3] , [1,2,3]);
-assertArraysEqual([1,2,3] , [1,2,'3']);
-assertArraysEqual([1,2,3] , [1,2,3,5]);
-assertArraysEqual([1,2,3] , [1,5,3]);
-assertArraysEqual([1,2,'3'] , [1,2,'3']);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1,2,3] , [1,2,3]);
+assertArraysEqual([1,2,3] , [1,2,'3']);
+assertArraysEqual([1,2,3] , [1,2,3,5]);
+assertArraysEqual([1,2,3] , [1,5,3]);
+assertArraysEqual([1,2,'3'] , [1,2,'3']);
