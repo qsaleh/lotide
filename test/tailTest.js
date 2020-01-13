07a90957dc1c6f94e659347cab5cf633feda9dd1
qsaleh <qmsaleh@gmail.com>
2020-01-13 21:24:01 +0000

    updated middleTest to use mocha/chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,17 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+const words = ['Hello', 'Lighthouse', 'Labs'];
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(words).length, 2);
+  });
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(words)[0], 'Lighthouse');
+  });
+  it("returns 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(words)[1], 'Labs');
+  });
+
+});
